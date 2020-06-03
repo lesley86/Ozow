c924b6cebfccd368d01004d8c860499417cf2767
Lesley Simons <7706813+lesley86@users.noreply.github.com>
2020-06-03 13:07:48 +0200

    adding logging for alphbet sorter

--- Ozow.TextSort.CosnoleHost/Program.cs
+++ Ozow.TextSort.CosnoleHost/Program.cs
@@ -9,7 +9,8 @@ namespace Ozow.TextSort.CosnoleHost
         static void Main(string[] args)
         {
 
-            var input = args[0];
+            Console.WriteLine("Please enter the string you wish to sort.");
+            var input = Console.ReadLine();
             var sortedString = new CustomTextSort().Sort(input);
             Console.WriteLine(sortedString);
         }
